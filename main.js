e1b1c286b66be5f9e6c2004c78d726376ce04f9f
Gabriel-Dupla <gabrielduplacontato@gmail.com>
2023-03-23 16:45:30 -0300

    Aula-3 Código

--- main.js
+++ main.js
@@ -1,23 +1,54 @@
+//Pegando o id do formulário e lista de itens 
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
+//Pegando elementos guardados no local storage e tornando valor ou cria array vazio para os itens
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
+//Itens do localStorage colocados na tela
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+
+} )
+
+//Recebendo e criando os itens
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
 
+    //Cria objeto item atual
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+
+    }
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    nome.value = "" ;
+    quantidade.value = "";
 })
 
-function criaElemento(nome, quantidade) {
+//Criando os elementos
+function criaElemento(item) {
+    //Cria uma elemento de tag <li> para o HTML sem alterar o HTML.
     const novoItem = document.createElement('li');
+    //Adiciona a classe item para a tag <li> recém criado no HTML, exemplo linha 29.
     novoItem.classList.add("item");
 
+    //Cria uma elemento de tag <strong> para o HTML.
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
-
+    /*Coloca a tag <strong> dentro da tag <li>, index.html, exemplo linha 29.*/
+    numeroItem.innerHTML = item.quantidade;
+    //
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+
+    novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
-
 }
